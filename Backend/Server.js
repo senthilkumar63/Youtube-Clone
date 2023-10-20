09acb8d6f2dced7605bcd8964c987f3afd464dae
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-10-20 07:23:19 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,14 +4,29 @@ const express =require('express')
 const notes = require ('./data/notes.js')
 const dotenv = require('dotenv')
 const cors = require ('cors')
-const {test} = require('./controllers/authController')
+// const {test} = require('./controllers/authController')
 const {mongoose} = require('mongoose')
-const userRoutes = require('./routes/userRoutes')
+const authRoutes = require('./routes/auth')
+const passportSetup = require('./passport')
+const passport = require('passport')
+const session = require('express-session');
+const cookieSession = require('cookie-session')
+
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401)
+}
 
 const app = express();
+app.use(cookieSession({name:'session', keys:['youtube'], maxAge:24*60*60*100}))
+app.use(session({secret: 'youtube', resave: false,
+saveUninitialized: true,
+cookie: { secure: true }}))
+app.use(passport.initialize())
+app.use(passport.session())
 dotenv.config();
-app.use(cors());
-// app.use(express.json)
+app.use(cors({origin:'http://localhost:3000',methods:'GET,POST,PUT,DELETE', credentials:true}));
+app.use('/auth', authRoutes)
+app.use(express.json())
 
 
 
@@ -19,23 +34,47 @@ mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Database connected"))
 .catch((err) => console.log("Databse not connected",err))
 
-app.use('/', require('./routes/authRoutes'))
-
-app.use(express.json())
+// app.use('/', require('./routes/authRoutes'))
 
 
-app.use('/api/users', userRoutes)
 
-app.get('/', (req, res) => {
-    res.send("Api is running")
-})
+
+// app.use('/api/users', userRoutes)
+
+// app.get('/', (req, res) => {
+//     res.send("Api is running")
+// })
 app.get('/api/notes', (req, res) =>{
     res.json(notes);
 })
 app.get('/api/notes/:videoId', (req,res) =>{
     const item = notes.find((n) => n.videoId === req.params.videoId);
-    
     res.send(item)
 })
+
+
+// app.get('/', (req,res) =>{
+//     res.send('<a href="/auth/google">Authenticate with google</a>')
+// })
+// app.get('/protected', isLoggedIn, (req,res) =>{
+//     res.send(`Hi ${req.user.displayName}`)
+// })
+// app.get('/logout', (req, res) => {
+//     req.logOut()
+//     req.session.destroy()
+//     req.send('Goodbye!')
+// })
+// app.get('/auth/google', 
+//     passport.authenticate('google', {scope:['email', 'profile']}))
+
+// app.get('/google/callback', passport.authenticate('google', {
+//     successRedirect: '/protected',
+//     failureRedirect: '/auth/failure'
+// }))
+// app.get('/auth/failure', (req,res) => {
+//     res.send("Something went wrong")
+// })
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Server Started running"))
