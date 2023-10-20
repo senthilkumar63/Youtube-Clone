afad8cad5811fb710543c1463f6f6ec086e4ea89
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-10-20 07:24:52 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect,useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import {Navbar , SearchFeed, ChannelDetail, VideoDetail, Feed, Chips} from './components';
 import Login from './Pages/Login';
@@ -7,24 +8,52 @@ import Register from './Pages/Register';
 import axios from 'axios'
 import {Toaster} from 'react-hot-toast'
 
-axios.defaults.baseURL = 'http://localhost:5000'
-axios.defaults.withCredentials = true
+// axios.defaults.baseURL = 'http://localhost:5000'
+// axios.defaults.withCredentials = true
 const App = () => {
+  const [user, setUser] = useState(null)
+ useEffect(() => {
+  const getUser = () => {
+    fetch("http://localhost:5000/auth/login/success",{
+      method:'GET',
+      credentials:'include',
+      headers:{
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+        'Access-Control-Allow-Credentials': true,
+      }
+    }).then(response =>{
+      if(response.status ===200) return response.json()
+      throw new Error ('Authentication has been failed')
+    }).then(resObject=>{
+      setUser(resObject.user)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  } 
+  getUser()
+   }
+   
+ ,[])
+ console.log(user)
+ 
+  // const user = false
   return (
     <BrowserRouter>
       <Box sx={{}}>
-        <Navbar/>
-        <Toaster position='top-center' toastOptions={{duration :2000}}/>
+        <Navbar user={user}/>
+        {/* <Toaster position='top-center' toastOptions={{duration :2000}}/> */}
         <Chips/>
         <br/>
         <Routes>
-            <Route path='/' exact element={<Feed />}/>
+            <Route path='/' exact element={ <Feed />}/>
             <Route path='/video/:id' element={<VideoDetail />}/>
             <Route path='/channel/:id' element={<ChannelDetail />}/>
             <Route path='/search/:searchTerm' element={<SearchFeed />}/>
             <Route path='/register' element={<Register/>}/>
-            <Route path='/login' element={<Login/>}/>
+            <Route path='/login' element={user ? <Navigate to='/'/>:<Login/>}/>
         </Routes>
+        
       </Box>
     </BrowserRouter>
   )
